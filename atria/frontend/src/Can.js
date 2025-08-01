13993a9307e870de7338a81199861be4be486db5
Alexandru Popescu <romanian_paf@yahoo.com>
2025-08-01 16:51:52 +0300

    Inițial

--- atria/frontend/src/Can.js
+++
@@ -1,5 +0,0 @@
-import { createContextualCan } from "@casl/react";
-import React from "react";
-
-export const AbilityContext = React.createContext();
-export const Can = createContextualCan(AbilityContext.Consumer);
